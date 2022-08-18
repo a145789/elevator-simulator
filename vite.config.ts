d2ba611b0285f090f7f0daba43ff917d23824bd0
a145789 <2091927351@qq.com>
2022-08-18 22:43:51 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import Unocss from 'unocss/vite'
+
+export default defineConfig({
+  plugins: [Unocss(), solidPlugin()],
+  server: {
+    port: 3000
+  },
+  build: {
+    target: 'esnext'
+  }
+})

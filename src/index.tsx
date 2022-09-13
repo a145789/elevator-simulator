3de61c2e379595c02a9976f6a506f7ed3fd73933
clencat <37403253+a145789@users.noreply.github.com>
2022-09-13 19:17:24 +0800

    feat: add global css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 import { render } from 'solid-js/web'
 import 'uno.css'
 import '@unocss/reset/eric-meyer.css'
+import './index.css'
 import App from './App'
 
 render(() => <App />, document.getElementById('root') as HTMLElement)

d9a063ad485d9b3eb0be4728df937f12db19cbf1
clencat <37403253+a145789@users.noreply.github.com>
2022-09-19 11:50:27 +0800

    feat: 进入按钮动画

--- src/components/GetInButton.tsx
+++ src/components/GetInButton.tsx
@@ -1,50 +1,36 @@
-import { Component, createEffect, createSignal, onMount } from "solid-js"
+import { Component, createMemo } from "solid-js"
 import { DOOR_ACTION_TIME } from "../constants"
 
 const GetInButton: Component<{
   isClose: boolean
   emitGetInElevator: () => void
 }> = (props) => {
-  const [transitionClose, setTransitionClose] = createSignal(true)
-  onMount(() => {
-    setTimeout(() => {
-      createEffect(() => {
-        setTransitionClose(props.isClose)
-      })
-    })
+  const transitionConfigStyle = createMemo(() => {
+    return {
+      "transition-delay": `${props.isClose ? DOOR_ACTION_TIME / 3 : 0}ms`,
+      "transition-duration": `${
+        props.isClose ? DOOR_ACTION_TIME / 2 : DOOR_ACTION_TIME / 2
+      }ms`,
+    }
   })
 
   return (
     <div class="top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 absolute overflow-hidden">
       <div
-        class="h-full w-1/2 bg-#fff absolute z-20 transition-all"
-        style={{
-          "transition-delay": `${
-            transitionClose() ? 0 : DOOR_ACTION_TIME / 4
-          }ms`,
-          "transition-duration": `${
-            transitionClose() ? DOOR_ACTION_TIME / 4 : DOOR_ACTION_TIME
-          }ms`,
-        }}
+        class="h-full w-1/2 bg-#fff absolute z-20 transition-all ease"
+        style={transitionConfigStyle()}
         classList={{
-          "left-0": transitionClose(),
-          "-left-1/2": !transitionClose(),
+          "left-0": props.isClose,
+          "-left-1/2": !props.isClose,
         }}
       />
       <div
-        class="h-full w-1/2 bg-#fff absolute z-20 transition-all"
+        class="h-full w-1/2 bg-#fff absolute z-20 transition-all ease"
         classList={{
-          "right-0": transitionClose(),
-          "-right-1/2": !transitionClose(),
+          "right-0": props.isClose,
+          "-right-1/2": !props.isClose,
         }}
-        style={{
-          "transition-delay": `${
-            transitionClose() ? 0 : DOOR_ACTION_TIME / 4
-          }ms`,
-          "transition-duration": `${
-            transitionClose() ? DOOR_ACTION_TIME / 4 : DOOR_ACTION_TIME
-          }ms`,
-        }}
+        style={transitionConfigStyle()}
       />
       <div
         class="box-border p-4px text-center border"
